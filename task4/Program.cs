62d9f7f6c86f38737fa276fc898996fb3ff8afae
ArtemUb <savenkoam@gmail.com>
2023-04-24 20:18:02 +0900

    task 4 done

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] num = new int[3];
+
+Console.WriteLine("Введите первое число");
+num[0] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+num[1] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+num[2] = Convert.ToInt32(Console.ReadLine());
+
+int max = num[0];
+
+for (int i = 1; i < num.Length; i++)
+{
+    if (max < num[i])
+    {
+        max = num[i];
+    }
+}
+
+Console.WriteLine("Max = {0:D}", max);
