b9d27a0686661e0abf808812cf381e1e3fdec59c
ArtemUb <savenkoam@gmail.com>
2023-04-24 14:08:39 +0900

    done task 2 file

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Max a: {0:D}", a);
+}
+else if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine("Max b: {0:D}", b);
+}
+
